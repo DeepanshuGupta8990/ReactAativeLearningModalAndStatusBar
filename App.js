90ff5699a11a464fe73ff4efc736a96153e3247e
Deepanshu Gupta <deepanshugupta899@gmail.com>
2023-08-01 17:53:23 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Modal,Dimensions, Button,ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [modal,setModal] = useState(true)
+  const { width, height } = Dimensions.get('window');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello Deepanshu Gupta</Text>
+      <StatusBar backgroundColor='red' barStyle='light-content' hidden={true}/>
+      <Button onPress={()=>{setModal(true)}} title='OpenModal'/>
+      <ActivityIndicator/>
+      <ActivityIndicator size='large'/>
+      <ActivityIndicator size='large' color='red'/>
+      <ActivityIndicator size='large' color='red' animating={false}/>
+      <Modal visible={modal} onRequestClose={()=>{setModal(false)}} animationType='slide'>
+        <View  style={[styles.modalstyle,{width:width,height:height}]}> 
+      <Text style={styles.modalText}>
+      Hello brother
+      </Text>
+      <Button onPress={()=>{setModal(false)}} title='CloseModal'/>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -17,4 +33,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  modalstyle: {
+    flex:1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalText:{
+    color:'white',
+  }
 });
